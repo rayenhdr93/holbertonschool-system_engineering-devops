7536edb35d080f0572fc3d5c9d34fd171055eb88
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-16 20:26:38 +0100

    pycodestyle fixed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,19 +9,19 @@ import csv
 if __name__ == "__main__":
     '''fixin the module'''
     list = []
-    alltodos = 0
-    comptodos = 0
-    URL1 = "https://jsonplaceholder.typicode.com/todos/"
+    alltod = 0
+    comptod = 0
+    URL1 = "https://jsonplaceholder.typicode.com/tod/"
     URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response1 = get(URL1)
     response2 = get(URL2)
-    todos = response1.json()
+    tod = response1.json()
     users = response2.json()
     u = users["username"]
     f = open('./{}.csv'.format(argv[1]), 'w')
     writer = csv.writer(f)
-    for i in range(0, len(todos)):
-        if (((todos[i]["userId"]) == int(argv[1]))):
+    for i in range(0, len(tod)):
+        if (((tod[i]["userId"]) == int(argv[1]))):
             writer.writerow([str(
-                todos[i]["userId"]), u, todos[i]["completed"], todos[i]["title"]])
+                tod[i]["userId"]), u, tod[i]["completed"], tod[i]["title"]])
     f.close()
