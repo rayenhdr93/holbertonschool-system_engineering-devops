ade8739befa62c32a193eee32d84b1a6912baf87
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-16 19:48:52 +0100

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,23 +6,23 @@ from requests import get
 from sys import argv
 
 if __name__ == "__main__":
-	'''fixin the module'''
-	list = []
-	alltodos = 0
-	comptodos = 0
-	URL1 = "https://jsonplaceholder.typicode.com/todos/"
-	URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-	response1 = get(URL1)
-	response2 = get(URL2)
-	todos = response1.json()
-	users = response2.json()
-	for i in range(0, len(todos)):
-		if (((todos[i]["userId"]) == int(argv[1]))):
-			alltodos += 1
-			if (todos[i]["completed"]):
-				comptodos += 1
-				list = list + [(todos[i]["title"])]
-	print("Employee {} is done with tasks({}/{}):".format(
-		users["name"], comptodos, alltodos))
-	for i in range(0, len(list)):
-		print("\t", list[i])
+    '''fixin the module'''
+    list = []
+    alltodos = 0
+    comptodos = 0
+    URL1 = "https://jsonplaceholder.typicode.com/todos/"
+    URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response1 = get(URL1)
+    response2 = get(URL2)
+    todos = response1.json()
+    users = response2.json()
+    for i in range(0, len(todos)):
+        if (((todos[i]["userId"]) == int(argv[1]))):
+            alltodos += 1
+            if (todos[i]["completed"]):
+                comptodos += 1
+                list = list + [(todos[i]["title"])]
+    print("Employee {} is done with tasks({}/{}):".format(
+        users["name"], comptodos, alltodos))
+    for i in range(0, len(list)):
+        print("\t", list[i])
