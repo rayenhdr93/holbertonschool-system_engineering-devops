2081cf0ad00e9012f910ac32120466c1d88553c4
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-16 22:02:49 +0100

    3- fixed

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,23 +9,24 @@ import json
 
 if __name__ == "__main__":
     '''fixin the module'''
-    list = []
+
     URL1 = "https://jsonplaceholder.typicode.com/todos/"
     response1 = get(URL1)
     tod = response1.json()
-    for x in range(1, 10):
+    dict2 = {}
+    for x in range(1, 11):
         URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(str(x))
         response2 = get(URL2)
         users = response2.json()
         u = users["username"]
+        list = []
         for i in range(0, len(tod)):
             if (((tod[i]["userId"]) == int(x))):
                 dict = {}
+                dict["username"] = u
                 dict["task"] = tod[i]["title"]
                 dict["completed"] = tod[i]["completed"]
-                dict["username"] = u
                 list.append(dict)
-        dict2 = {}
         dict2[x] = list
         with open("todo_all_employees.json".format(x), 'a') as f:
             json.dump(dict2, f)
