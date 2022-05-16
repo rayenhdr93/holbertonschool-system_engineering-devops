0ad2bc8c83da59d03a44d1434f05dff1a4b2e9e3
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-16 19:34:27 +0100

    Readme 0-

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from requests import get
+import json
+from sys import argv
+list = []
+alltodos = 0
+comptodos = 0
+URL1 = "https://jsonplaceholder.typicode.com/todos/"
+URL2 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+response1 = get(URL1)
+response2 = get(URL2)
+todos = response1.json()
+users = response2.json()
+for i in range(0, len(todos)):
+    if (((todos[i]["userId"]) == int(argv[1]))):
+        alltodos += 1
+        if (todos[i]["completed"]):
+            comptodos += 1
+            list = list + [(todos[i]["title"])]
+print("Employee {} is done with tasks({}/{})".format(
+    users["name"], comptodos, alltodos))
+for i in range(0, len(list)):
+    print("     ", list[i])
