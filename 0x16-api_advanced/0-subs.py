2b8f4eebb14eb19476b628a12338a5398f564a29
rayenhdr93 <hedrirayen69@gmail.com>
2022-07-08 14:29:48 +0100

    0-

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+'''pycode'''
+
+
+def number_of_subscribers(subreddit):
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},)
+    x = str(sub_info)
+    if x[:16] == '<Response [404]>':
+        return 0
+    else:
+        x = sub_info.json().get("data").get("subscribers")
+    return (x)
