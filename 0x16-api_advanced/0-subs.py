d5c3abfb38731fcc233a370337c8c064edb77d1d
rayenhdr93 <hedrirayen69@gmail.com>
2022-07-08 14:31:00 +0100

    0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    '''def'''
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},)
